244fded76586137b00bf67563668ab6f7a80682b
Roman Imankulov <roman.imankulov@gmail.com>
2014-05-21 13:49:43 +0300

    Misc updates
    - API query function uses local cache
    - Extend users with userinfo from API
    - Extend corpus with more trivial metrics

--- antivandal.py
+++ antivandal.py
@@ -32,23 +32,35 @@ MongoDB document schema
 [1]: http://www.uni-weimar.de/en/media/chairs/webis/research/corpora/corpus-pan-wvc-10/
 [2]: http://www.uni-weimar.de/en/media/chairs/webis/research/corpora/corpus-pan-wvc-11/
 """
+from __future__ import division
+import re
 import os
+import nltk
 import time
 import pymongo
 import requests
 import datetime
+import string
+import lzw
+import urllib
 from tqdm import tqdm
 from csv import DictReader, DictWriter
 
 # env variables
 MONGODB_URL = os.getenv('MONGODB_URL', 'mongodb://localhost:27017/vandal')
 MONGODB_CORPUS_COLLECTION = os.getenv('MONGODB_CORPUS_COLLECTION', 'corpus')
-
+MONGODB_HTTPCACHE_COLLECTION = os.getenv('MONGODB_HTTPCACHE_COLLECTION', 'httpcache')
+MONGODB_USERS_COLLECTION = os.getenv('MONGODB_USERS_COLLECTION', 'users')
 
 # create mongodb connection, database and collection
 conn = pymongo.MongoClient(MONGODB_URL)
 db = conn.get_default_database()
 corpus = db[MONGODB_CORPUS_COLLECTION]
+httpcache = db[MONGODB_HTTPCACHE_COLLECTION]
+users = db[MONGODB_USERS_COLLECTION]
+
+# misc constants
+re_ip = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
 
 
 def ensure_index():
@@ -57,6 +69,9 @@ def ensure_index():
     """
     corpus.ensure_index([('ds', pymongo.ASCENDING), ('editid', pymongo.ASCENDING)],
                         unique=True, name='unique_id')
+    corpus.ensure_index([('vanalism', pymongo.ASCENDING)], name='vandalism')
+    httpcache.ensure_index([('key', pymongo.ASCENDING)], name='cache_key')
+    users.ensure_index([('name', pymongo.ASCENDING)], name='name')
 
 
 def import_corpus_2010():
@@ -130,7 +145,7 @@ def export_corpus(filename, exclude=None):
                     "oldrevision" and "newrevision" records
     """
     if exclude is None:
-        exclude = ['oldrevision', 'newrevision']
+        exclude = ['oldrevision', 'newrevision', 'diff_word', 'neg_diff_word']
 
     exclude = set(exclude)
     exclude.add('_id')
@@ -169,10 +184,77 @@ def extend_with_text_metrics(record):
     """
     old_rev = record['oldrevision']
     new_rev = record['newrevision']
+    old_rev_set = set(nltk.word_tokenize(old_rev))
+    new_rev_set = set(nltk.word_tokenize(new_rev))
+
+    diff_set = sorted(new_rev_set.difference(old_rev_set))
+    diff_word = u' '.join(diff_set)
+
+
+    neg_diff_set = sorted(old_rev_set.difference(new_rev_set))
+    neg_diff_word = u' '.join(neg_diff_set)
+
+    # as described by Santigo M. Mola Velasco
+    total_len = len(diff_word) + 1
+    upper_len = sum(c in uppercase for c in diff_word) + 1
+    lower_len = sum(c in uppercase for c in diff_word) + 1
+    digits_len = sum(c in digits for c in diff_word) + 1
+    alnum_len = sum(c in alphanum for c in diff_word) + 1
+    compressed_len = len(list(lzw.compress(diff_word.encode('utf8')))) + 1
+
+    neg_total_len = len(neg_diff_word) + 1
+    neg_upper_len = sum(c in uppercase for c in neg_diff_word) + 1
+    neg_lower_len = sum(c in uppercase for c in neg_diff_word) + 1
+    neg_digits_len = sum(c in digits for c in neg_diff_word) + 1
+    neg_alnum_len = sum(c in alphanum for c in neg_diff_word) + 1
+    neg_compressed_len = len(list(lzw.compress(neg_diff_word.encode('utf8')))) + 1
+
     return {
         'difflen': len(new_rev) - len(old_rev),
         'commentlen': len(record['editcomment']),
+        'sz_ratio': (len(new_rev) + 1) / (len(old_rev) + 1),
+
+        # positive diff data
+        'ul_ratio': upper_len / lower_len,
+        'u_ratio': upper_len / total_len,
+        'd_ratio': digits_len / total_len,
+        'non_alnum_ratio': (total_len - alnum_len) / total_len,
+        'compressibility': total_len / compressed_len,
+        'longest_word': max(len(w) for w in diff_set) if diff_set else 0,
+        'longest_seq': find_longest_seq(diff_word),
+        'diff_word': diff_word,
+
+        # negative diff data
+        'neg_ul_ratio': neg_upper_len / neg_lower_len,
+        'neg_u_ratio': neg_upper_len / neg_total_len,
+        'neg_d_ratio': neg_digits_len / neg_total_len,
+        'neg_non_alnum_ratio': (neg_total_len - neg_alnum_len) / neg_total_len,
+        'neg_compressibility': neg_total_len / neg_compressed_len,
+        'neg_longest_word': max(len(w) for w in neg_diff_set) if neg_diff_set else 0,
+        'neg_longest_seq': find_longest_seq(neg_diff_word),
+        'neg_diff_word': neg_diff_word,
     }
+
+
+uppercase = set(string.uppercase)
+lowercase = set(string.lowercase)
+digits = set(string.digits)
+alphanum = set(string.ascii_letters + string.digits)
+
+
+def find_longest_seq(string):
+    prev = None
+    sz = 0
+    max_sz = 0
+    for c in string:
+        if c == prev:
+            sz += 1
+        else:
+            prev = c
+            max_sz = max(sz, max_sz)
+            sz = 1
+    max_sz = max(sz, max_sz)
+    return max_sz
 
 
 #--- Wikipedia API helpers
@@ -181,8 +263,26 @@ def api_request(action='query', **kw):
     headers = {'User-Agent': 'AntiVandal Wikipedia API client (https://github.com/imankulov/pyconru2014)'}
     kw.update(format='json')
     base_url = 'http://en.wikipedia.org/w/api.php'
-    resp = requests.get(base_url, headers=headers, params=dict(action=action, **kw))
-    return resp.json()
+
+    # convert lists to mediawiki standart representation ("|"-separated string)
+    for k, v in kw.items():
+        if isinstance(v, list):
+            kw[k] = '|'.join(v)
+
+    # GET parameters for HTTP request
+    params = dict(action=action, **kw)
+
+    # cache hit
+    cache_key = urllib.urlencode(sorted(params.iteritems()))
+    cache_result = httpcache.find_one({'key': cache_key})
+    if cache_result:
+        return cache_result['val']
+
+    # cache miss
+    resp = requests.get(base_url, headers=headers, params=params)
+    json_resp = resp.json()
+    httpcache.insert({'key': cache_key, 'val': json_resp})
+    return json_resp
 
 
 def get_page_revisions(page_id, revision_id):
@@ -207,6 +307,28 @@ def get_page_revisions(page_id, revision_id):
             break
         rvcontinue = resp['query-continue']['revisions']['rvcontinue']
     return ret
+
+
+def fill_editors_info():
+    usprop = 'blockinfo|groups|editcount|registration|emailable|gender'
+
+    cursor = corpus.find({}, fields=['editor'])
+    seen = set()
+
+    current = set()
+    for record in tqdm(cursor, total=corpus.count()):
+        editor = record['editor']
+        if re_ip.match(editor) or editor in seen:
+            continue
+        current.add(editor)
+        if len(current) >= 50:
+            seen.update(current)
+            result = api_request(list='users', ususers=current, usprop=usprop)
+            users.insert(result['query']['users'])
+            current = set()
+    if current:
+        result = api_request(list='users', ususers=current, usprop=usprop)
+        users.insert(result['query']['users'])
 
 
 #--- Converters
